0606bedb020425ae3cb53b4156c7c164c51be5e8
Aron Balog <aronbalog@gmail.com>
2021-05-03 11:30:40 +0200

    Added dashboard and playlists view

--- AudioPlayer/ContentView.swift
+++ AudioPlayer/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView(selection: .constant(0),
+                content:  {
+                    Text("Tab Content 1").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 1")/*@END_MENU_TOKEN@*/ }.tag(1)
+                    
+                    Text("Tab Content 2").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 2")/*@END_MENU_TOKEN@*/ }.tag(2)
+                })
     }
 }
 
